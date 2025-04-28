b4339962883aa3477168ba5d6c5ecd01e54d36c8
Stas Osk <stas-stas.stanislav@mail.ru>
2025-04-28 08:58:15 +0300

    удаление лишнего

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,6 @@
-import AppLayout from '../layouts/Layout'
 
 const MainPage = () => {
-  return (
-    <AppLayout>
-        <div className='max-w-7xl px-6 lg:px-8'>
-            Нихуя себе
-        </div>
-    </AppLayout>
-  )
+  return <div></div>
 }
 
-export default MainPage
+export default MainPage;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,16 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import MainPage from '../pages/MainPage';
-import SecondPage from '../pages/SecondPage';
-import ErrorPage from '../pages/ErrorPage';
-import StoryBook from '../pages/StoryBook';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" index element={<MainPage />} />
-      <Route path="/second" index element={<SecondPage />} />
-      <Route path="/storybook" index element={<StoryBook /> } />
-      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 };
